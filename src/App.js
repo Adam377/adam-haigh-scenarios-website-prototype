a7a9e8bbef4f28ad299ae005ce70da606e32e122
adamhaigh <adam.haigh@takepayments.com>
2023-01-02 15:29:32 +0000

    Finished Content & Image Block styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Masthead from './components/Layout/Masthead';
 import Scenarios from './components/Screens/Scenarios';
 import AboutMe from './components/Screens/AboutMe';
 import ContentOnlyBlock from './components/Layout/Blocks/ContentOnlyBlock';
+import ContentImageBlock from './components/Layout/Blocks/ContentImageBlock';
+
+import class319 from './assets/class319WBQ.JPG';
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
       <Masthead />
       <main>
         <Switch>
+          <Route path="/" exact>
+            <ContentImageBlock title="Welcome!" image={class319}>Nunc tincidunt arcu quis diam pharetra laoreet. Proin eget nulla lorem. Nullam et sagittis dui, nec vestibulum diam. Proin nec finibus justo. Donec ac vehicula dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras sit amet vehicula mauris. Quisque tempor convallis malesuada. Praesent sed velit id lorem ullamcorper interdum a et ligula. Cras gravida pretium odio, et faucibus sem blandit vitae. Pellentesque tincidunt fermentum nunc ut consequat. Sed varius, dolor ac vehicula egestas, enim ipsum congue ante, vitae dictum ligula arcu nec neque. Curabitur in quam ac nibh varius blandit et quis lorem. Duis at congue mi, et convallis velit. Quisque condimentum ante eu purus semper finibus. Morbi pellentesque eget mauris et finibus. Fusce rutrum purus et est tincidunt, at condimentum felis dignissim. Duis tempor lobortis porta. Cras ultricies est dui, et auctor nulla sagittis at. Nulla lobortis neque sapien, ut egestas nisl lobortis nec.</ContentImageBlock>
+          </Route>
           <Route path="/scenarios">
             <Scenarios />
           </Route>
@@ -20,7 +26,6 @@ function App() {
             <AboutMe />
           </Route>
         </Switch>
-        <ContentOnlyBlock title="Welcome!">Welcome to my site. Feel free to have a look around for scenarios!</ContentOnlyBlock>
       </main>
     </div>
   );
--- src/components/Layout/Blocks/ContentImageBlock.js
+++ src/components/Layout/Blocks/ContentImageBlock.js
@@ -3,9 +3,21 @@ import classes from './ContentImageBlock.module.css';
 
 const ContentImageBlock = (props) => {
     return (
-        <div>
-            Content Image Block!
-        </div>
+        <Fragment>
+            <div className={classes['content-image-block']}>
+                <div className={classes.container}>
+                    <img className={classes.image} src={props.image} />
+                    <div className={classes.content}>
+                        <div className={classes.title}>
+                            {props.title}
+                        </div>
+                        <div className={classes['inner-content']}>
+                            {props.children}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
     );
 };
 
